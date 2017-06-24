d9cf7b7ed435f19cf4068499cef216bffe4ecbbb
Chris Robertson <chrisxrobertson@gmail.com>
2017-06-24 19:00:45 +1000

    Add initial routes

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const express = require('express');
+const routes = require('./routes/api');
 
+const app = express();
+app.use('/api', routes);
+
+app.listen(process.env.port || 4000, function() {
+  console.log('Now listening for requests...');
+});
---
+++ routes/api.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+// Get a list of exchanges from the database
+router.get('/exchanges', function(req, res) {
+  res.send({type: 'GET'});
+});
+
+// Add a new exchange to the database
+router.post('/exchanges', function(req, res) {
+  res.send({type: 'POST'});
+});
+
+// Update an exchange in the database
+router.put('/exchanges/:id', function(req, res) {
+  res.send({type: 'PUT'});
+});
+
+// Delete an exchange in the database
+router.delete('/exchanges/:id', function(req, res) {
+  res.send({type: 'DELETE'});
+});
+
+module.exports = router;
